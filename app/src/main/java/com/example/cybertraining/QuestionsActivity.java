717ef8356f33e596f3cd4b3fbcf084e4604fc8d9
Inder241 <chera.bhupinder@gmail.com>
2023-03-22 22:23:54 +0000

    Implementation of the timer in QuestionsActivity.

--- app/src/main/java/com/example/cybertraining/QuestionsActivity.java
+++ app/src/main/java/com/example/cybertraining/QuestionsActivity.java
@@ -1,13 +1,31 @@
 package com.example.cybertraining;
 
+import static android.widget.LinearLayout.HORIZONTAL;
+
+import static com.example.cybertraining.DbQuery.g_catList;
+import static com.example.cybertraining.DbQuery.g_quesList;
+import static com.example.cybertraining.DbQuery.g_selected_cat_index;
+import static com.example.cybertraining.DbQuery.g_selected_test_index;
+import static com.example.cybertraining.DbQuery.g_testList;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.PagerSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class QuestionsActivity extends AppCompatActivity {
 
@@ -16,6 +34,7 @@ public class QuestionsActivity extends AppCompatActivity {
     private Button submitB, markB, clearSelB;
     private ImageButton prevQuestB, nextQuestB;
     private ImageView quesListB;
+    private int quesID;
 
 
     @Override
@@ -24,8 +43,19 @@ public class QuestionsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_questions);
 
         init();
-        QuestionsAdapter quesAdapter = new QuestionsAdapter(DbQuery.g_quesList);
+        QuestionsAdapter quesAdapter = new QuestionsAdapter(g_quesList);
         questionsView.setAdapter(quesAdapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        questionsView.setLayoutManager(layoutManager);
+
+        setSnapHelper();
+
+        setClickListener();
+
+        startTimer();
+
 
     }
 
@@ -41,7 +71,88 @@ public class QuestionsActivity extends AppCompatActivity {
         prevQuestB = findViewById(R.id.prev_quesB);
         nextQuestB = findViewById(R.id.next_quesB);
         quesListB = findViewById(R.id.ques_list_gridB);
+        quesID = 0;
 
+        tvQuesID.setText("1/" + String.valueOf(g_quesList.size())); //display itemID
+        catNameTV.setText(g_catList.get(g_selected_cat_index).getName()); //display name
+
+    }
+
+    private void setSnapHelper()
+    {
+        SnapHelper snapHelper = new PagerSnapHelper();
+        snapHelper.attachToRecyclerView(questionsView);
+
+        questionsView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                View view = snapHelper.findSnapView(recyclerView.getLayoutManager());
+                quesID = recyclerView.getLayoutManager().getPosition(view); //position of the view visible
+
+                tvQuesID.setText(String.valueOf(quesID + 1) + "/" + String.valueOf(g_quesList.size()));
+
+
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+
+    }
+
+    private void setClickListener()
+    {
+        prevQuestB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (quesID > 0 )    //not the first question
+                {
+                    questionsView.smoothScrollToPosition(quesID -1);
+                }
+            }
+        });
+
+        nextQuestB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (quesID < g_quesList.size() -1)
+                {
+                    questionsView.smoothScrollToPosition(quesID + 1);
+                }
+            }
+        });
+    }
+
+    private void startTimer()
+    {
+        long totalTime = g_testList.get(g_selected_test_index).getTime()*60*1000;
+
+        CountDownTimer timer = new CountDownTimer(totalTime + 1000, 1000) {
+            @Override
+            public void onTick(long remainingTime) {
+                String time = String.format("%02d:%02d min",
+                        TimeUnit.MILLISECONDS.toMinutes(remainingTime),
+                        TimeUnit.MILLISECONDS.toSeconds(remainingTime) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(remainingTime))
+
+                        );
+
+                timerTV.setText(time);
+            }
+
+            @Override
+            public void onFinish() {
+
+
+            }
+        };
+        timer.start();
 
     }
 }
